f75e399c564d77e5bfa0da060a1f391a50849106
AleksandrZachkevich <aleksdark13@gmail.com>
2022-05-23 10:31:36 +0300

    BEM

--- main.js
+++ main.js
@@ -2,33 +2,31 @@
    Component : Menu
  ============================================================================ */
 var menuHandle = function () {
-  var body = document.body,
-      component = document.getElementsByClassName('main-navigation')[0],
-      button = document.querySelector(".main-navigation").querySelector('.nav-toggle'),
-      menu = document.querySelector(".main-navigation").querySelector('.menu-wrapper');
+    var body = document.body,
+        component = document.getElementsByClassName('navigation')[0],
+        button = document.querySelector(".navigation").querySelector('.nav-toggle'),
+        menu = document.querySelector(".navigation").querySelector('.nav-toggle__bar-wrapper');
 
-document.addEventListener('click', function(e) {
-    var target = e.target,
-        targetClassname = target.className;
+    document.addEventListener('click', function (e) {
+        var target = e.target,
+            targetClassname = target.className;
 
-    if (targetClassname === button.className || target.closest('.nav-toggle') !==null) {
-      if (!component.classList.contains('open')) {
-        component.classList.add('open');
-        body.classList.add('menu-opened');
-      } else {
-        component.classList.remove('open');
-        body.classList.remove('menu-opened');
-      }
-    }
-    else if (target.closest(".menu-wrapper")!==null) {
-      e.stopPropagation();
-    }
-    else {
-      component.classList.remove('open');
-      body.classList.remove('menu-opened');
-    }
-  });
+        if (targetClassname === button.className || target.closest('.nav-toggle') !== null) {
+            if (!component.classList.contains('navigation-open')) {
+                component.classList.add('navigation-open');
+                body.classList.add('menu-opened');
+            } else {
+                component.classList.remove('navigation-open');
+                body.classList.remove('menu-opened');
+            }
+        } else if (target.closest("nav-toggle__bar-wrapper") !== null) {
+            e.stopPropagation();
+        } else {
+            component.classList.remove('open');
+            body.classList.remove('menu-opened');
+        }
+    });
 };
 
 
-  document.addEventListener("DOMContentLoaded",menuHandle)
+document.addEventListener("DOMContentLoaded", menuHandle)
