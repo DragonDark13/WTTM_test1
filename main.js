995744a5bfb97663e43121c104f2cf0116997a6f
AleksandrZachkevich <aleksdark13@gmail.com>
2022-05-23 11:09:26 +0300

    ES6

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
-/* ==========================================================================
-   Component : Menu
- ============================================================================ */
-var menuHandle = function () {
-    var body = document.body,
-        component = document.getElementsByClassName('navigation')[0],
+const openClassName = 'navigation-open';
+
+const menuHandle = () => {
+    let component = document.getElementsByClassName('navigation')[0],
         button = document.querySelector(".navigation").querySelector('.nav-toggle'),
-        menu = document.querySelector(".navigation").querySelector('.nav-toggle__bar-wrapper');
+        componentClassList = component.classList;
 
     document.addEventListener('click', function (e) {
-        var target = e.target,
+        let target = e.target,
             targetClassname = target.className;
 
+        const closeButton = () => {
+            componentClassList.remove(openClassName);
+        }
+
         if (targetClassname === button.className || target.closest('.nav-toggle') !== null) {
-            if (!component.classList.contains('navigation-open')) {
-                component.classList.add('navigation-open');
-                body.classList.add('menu-opened');
+            if (!componentClassList.contains(openClassName)) {
+                componentClassList.add(openClassName);
             } else {
-                component.classList.remove('navigation-open');
-                body.classList.remove('menu-opened');
+                closeButton();
             }
         } else if (target.closest("nav-toggle__bar-wrapper") !== null) {
             e.stopPropagation();
         } else {
-            component.classList.remove('open');
-            body.classList.remove('menu-opened');
+            closeButton();
         }
     });
 };
 
 
-document.addEventListener("DOMContentLoaded", menuHandle)
+menuHandle()
